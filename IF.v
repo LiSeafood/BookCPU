2d17ec02b177f9e2aa3f4e7cc9d5d2135d3b6676
LiSeafood <1656250189@qq.com>
2023-12-14 19:45:23 +0800

    2023.12.14 19:45 首次尝试使用git管理自己的代码，提交三份代码试试

---
+++ IF.v
@@ -0,0 +1,23 @@
+`include "defines.v"
+module IF(
+    input wire                  clk,//时钟信号
+    input wire                  rst,//复位信号
+    output reg[`InstAddrBus]    pc,
+    output reg                  ce
+);
+    always @ (posedge clk) begin
+        if (rst) begin
+            pc <= 32'h00000000;//复位时pc为0
+        end else begin
+            pc <= pc + 4'h4;//pc等于下一个pc
+        end
+    end
+
+    always @ (posedge clk) begin
+        if (rst) begin
+            ce <= 1'b0;//复位时禁用指令存储器
+        end else begin
+            ce <= 1'b1;//指令存储器可用
+        end
+    end
+endmodule
---
+++ regfile.v
@@ -0,0 +1,54 @@
+`include "defines.v"
+module regfile(
+    input wire clk,
+    input wire rst,
+    //读端口1
+    input wire re1,
+    input wire [4:0] raddr1,
+    output reg [31:0] rdata1,
+    //读端口2
+    input wire re2,
+    input wire [4:0] raddr2,
+    output reg [31:0] rdata2,
+    //写端口
+    input wire we,
+    input wire [4:0] waddr,
+    input wire [31:0] wdata
+);
+    reg [31:0] reg_array [31:0];//32个32位寄存器
+
+    // write
+    always @ (posedge clk) begin
+        if(rst == `RstDisable)begin
+          if (we && waddr!=5'b0) begin
+            reg_array[waddr] <= wdata;
+          end//不复位、可写、写入数据非空时写入
+        end
+    end
+
+    // read out 1
+    always @( *) begin
+        if(rst == `RstEnable || raddr1 ==5'b0)begin
+          rdata1 <= `zeroword;//复位或空地址则读出空数据
+        end else if((raddr1 ==waddr)&& we && re1)begin
+          rdata1<=wdata;//写后读，直接把写数据赋给读数据
+        end else if(re1)begin
+          rdata1<=reg_array[raddr1];//正常读取
+        end else begin
+          rdata1 <= `zeroword;
+        end
+    end
+
+    // read out2
+    always @( *) begin
+        if(rst == `RstEnable || raddr2 ==5'b0)begin
+          rdata2 <= `zeroword;//复位或空地址则读出空数据
+        end else if((raddr2 ==waddr)&& we && re2)begin
+          rdata2<=wdata;//写后读，直接把写数据赋给读数据
+        end else if(re2)begin
+          rdata2<=reg_array[raddr2];//正常读取
+        end else begin
+          rdata2 <= `zeroword;
+        end
+    end
+endmodule
